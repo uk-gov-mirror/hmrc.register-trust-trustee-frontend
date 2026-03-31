77e879b8785fe353e6f4d08c81c24bc94fa9b23c
Mithra Balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2026-03-31 09:40:00 +0100

    DDCE-8272 - updating deps (#229)
    * DDCE-8272 - updating deps
    
    * DDCE-8272 - updating deps

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ import play.sbt.routes.RoutesKeys
 
 lazy val appName: String = "register-trust-trustee-frontend"
 
-ThisBuild / scalaVersion := "2.13.16"
+ThisBuild / scalaVersion := "2.13.18"
 ThisBuild / majorVersion := 0
 
 lazy val root = (project in file("."))
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,18 +2,18 @@ import sbt.*
 
 object AppDependencies {
 
-  private val playBootstrapVersion = "10.5.0"
+  private val playBootstrapVersion = "10.7.0"
 
   private lazy val compile = Seq(
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "12.29.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "12.32.0",
     "uk.gov.hmrc" %% "domain-play-30"                        % "11.0.0",
-    "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.4.0",
+    "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30"            % playBootstrapVersion
   )
 
   private lazy val test = Seq(
     "uk.gov.hmrc"          %% "bootstrap-test-play-30" % playBootstrapVersion,
-    "org.scalatestplus"    %% "scalacheck-1-18"        % "3.2.19.0",
+    "org.scalatestplus"    %% "scalacheck-1-19"        % "3.2.19.0",
     "io.github.wolfendale" %% "scalacheck-gen-regexp"  % "1.1.0"
   ).map(_ % Test)
 
